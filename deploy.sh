8e44224525de321e34e4867a7ab0b24028e918de
汪军梁 <1428637884@qq.com>
2022-10-27 23:15:48 +0800

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -188,11 +188,10 @@ if [[ "$GITHUB_EVENT_NAME" == "push" && "$GITHUB_REF" == "refs/heads/main" ]]; t
      header "rsync to the WHATWG server..."
       eval "$(ssh-agent -s)"
       echo "$SERVER_DEPLOY_KEY" | ssh-add -
-      echo ssh-add -l -E sha256
       mkdir -p ~/.ssh/ && echo "$SERVER $SERVER_PUBLIC_KEY" > ~/.ssh/known_hosts
      #No --delete as that would require extra care to not delete snapshots.
    # --chmod=D755,F644 means read-write for user, read-only for others.
-    rsync --verbose --archive --chmod=D755,F644 --compress \
+    rsync --verbose --archive --chmod=D700,F600 --compress \
        "$WEB_ROOT" "deploy@$SERVER:/home/www/htdocs/wangjunliang/"
 
 else 
