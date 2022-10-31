15799d7a7fa59509d5022607f07b4c41560d73c5
汪军梁 <1428637884@qq.com>
2022-10-31 11:44:57 +0800

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -194,6 +194,51 @@ if [[ "$GITHUB_EVENT_NAME" == "push" && "$GITHUB_REF" == "refs/heads/main" ]]; t
     # --chmod=D755,F644 means read-write for user, read-only for others.
     rsync --verbose --archive --chmod=D755,F644 --compress \
           "$WEB_ROOT" "root@$SERVER:/home/www/htdocs/wangjunliang/"
+
+# Change directory to the dest
+echo "==> Changing directory to '$WEB_ROOT' ..."
+
+cd $WEB_ROOT
+          # Get respository
+if [[ -z "$TARGET_REPO" ]]; then
+  REPOSITORY_NAME="${GITHUB_REPOSITORY}"
+else
+  REPOSITORY_NAME="$TARGET_REPO"
+fi
+
+# Get branch
+if [[ -z "$TARGET_BRANCH" ]]; then
+  DEPLOY_BRAN="gh-pages"
+else
+  DEPLOY_BRAN="$TARGET_BRANCH"
+fi
+
+# Final repository
+DEPLOY_REPO="https://username:${ACCESS_TOKEN}@github.com/${REPOSITORY_NAME}.git"
+if [ "$TARGET_LINK" ]; then
+  DEPLOY_REPO="$TARGET_LINK"
+fi
+
+git init
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+
+if [ -z "$(git status --porcelain)" ]; then
+    echo "The BUILD_DIR is setting error or nothing produced" && \
+    echo "Exiting..."
+    exit 0
+fi
+
+# Final repository
+if [[ -z "$COMMIT_MESSAGE" ]]; then
+  COMMIT_MESSAGE="Auto deploy from Github Actions"
+fi
+
+git add .
+git commit -m "$COMMIT_MESSAGE"
+git push --force $DEPLOY_REPO master:$DEPLOY_BRAN
+rm -fr .git
+
 else 
     header "Skipping deploy"
 fi
