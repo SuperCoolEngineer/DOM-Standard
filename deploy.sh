9d58abd98d0caed9a6fe4bb9bc385bb99462f74c
汪军梁 <1428637884@qq.com>
2022-10-31 14:00:32 +0800

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -215,9 +215,7 @@ fi
 
 # Final repository
 DEPLOY_REPO="https://username:${ACCESS_TOKEN}@github.com/${REPOSITORY_NAME}.git"
-if [ "$TARGET_LINK" ]; then
-  DEPLOY_REPO="$TARGET_LINK"
-fi
+
 
 git init
 git config user.name "${GITHUB_ACTOR}"
